4189945e66f520d4a7fa7191030e97a36ec6a9be
success899 <success899@naver.com>
2022-09-28 06:05:30 +0900

    GET POST test commit 2

--- memberapp/views.py
+++ memberapp/views.py
@@ -1,11 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from memberapp.models import HelloWorld
+
+
 # Create your views here.
 
 def hello_world(request):
 
     if request.method == "POST":
-        return render(request, 'memberapp/hello_world.html', context={'text': 'POST METHOD!'})
+
+        temp = request.POST.get('test_input')
+
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
+        return render(request, 'memberapp/hello_world.html', context={'hello_world_output': new_hello_world })
     else:
         return render(request, 'memberapp/hello_world.html', context={'text': 'GET METHOD!'})
