3ed4272aef70b6c6e5eb2838c0da910542349056
success899 <success899@naver.com>
2022-09-28 07:01:46 +0900

    Login Logout commit

--- memberapp/urls.py
+++ memberapp/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from memberapp.views import hello_world, MemberCreateView
@@ -8,4 +9,6 @@ urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
 
     path('create/', MemberCreateView.as_view(), name='create'),
+    path('login/', LoginView.as_view(template_name='memberapp/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
